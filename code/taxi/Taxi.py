3c06a675bc69731067cce7e000a94911f72fbcc3
JustinF97 <64065932+JustinF97@users.noreply.github.com>
2021-06-09 10:07:12 +0200

    Update Taxi.py
    Neuer Code/ Programmierstil änderen

--- code/taxi/Taxi.py
+++ code/taxi/Taxi.py
@@ -1,61 +1,75 @@
 import paho.mqtt.client as mqtt
 import json
 from random import *
+BROKER_ADDRESS = "test.mosquitto.org"
+PORT = 1883
+import time
+id = ""
+name = ""
 
-class car():
 
-    def  __init__(self, name, coordinaten,zahl,taxi):
-         self.name  = name
-         self.coordinaten = coordinaten
-         self.zahl = zahl
-         self.taxi = taxi
-        
-    cars = []
+
+print("gib einen namen ein:")
+name = input()
+
+def send(topic, object):
+    client = mqtt.Client("client")
+    client.connect(BROKER_ADDRESS, PORT)
+    name = object
+    print("Taxi")
+    def __init__(self, name):
+            self.name = name
+    print(" SEND Connected to MQTT Broker: " + BROKER_ADDRESS)
+    msg = str(name)
+    print(msg)
+    client.publish(topic, msg)
+    client.loop()
+
+def receive():
+    global id
+    temp = []
+    client = mqtt.Client("taxi")
+    client.connect(BROKER_ADDRESS, PORT)
+    def on_message(client, userdata, message):
+        msg = str(message.payload.decode("utf-8"))
+        print("message received: ", msg)
+        print("message topic: ", message.topic)
+        print(msg)
+        temp =  [message.topic,msg]
+        processing(temp)
+
 
     def on_connect(client, userdata, flags, rc):
+        print("receive Connected to MQTT Broker: " + BROKER_ADDRESS)
+        client.subscribe("hshl/mqtt_exercise/taxi/back",2)
+        client.subscribe("hshl/mqtt_exercise/taxi/"+str(id)+"/order/back", 2)
 
-        client.subscribe("hshl/mqtt_exercise/taxi", 2)
-
-        client.subscribe("hshl/mqtt_exercise/server", 2)
-        
-        client.subscribe("hshl/mqtt_exercise/taxi/get_postion", 2)
-        
-        client.subscribe("hshl/mqtt_exercise/taxi/back", 2)
-
-    def on_message(client, userdata, msg):
-        print(str(msg.payload))
-
-    client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
+    client.loop_start()
+    time.sleep(5)
+    client.loop_stop()
 
-    client.connect("test.mosquitto.org", 1883, 60)
-    
-    print("Taxi ID change")
-    taxi = input()
-    
-    zahly = randint(1, 4)
-    zahlx = randint(0, 4)
-    coordinates = str(zahly)+";"+str(zahlx)
+def processing(array):
+    global id
+    js = json.loads(array[1])
+    if "hshl/mqtt_exercise/taxi/back" == array[0] and str(js['name']) == str(name):
+        id = js['id']
+
+
+zahly = randint(0, 4)
+zahlx = randint(0, 4)
+coordinates = str(zahly)+";"+str(zahlx)
+
+
+def registration():
+    global name
     data = {
-     "id": "register",
-     "name": taxi,
-     "coordinates": coordinates,
-     "topic": "hshl/mqtt_exercise/taxi"
-     }
-    client.publish("hshl/mqtt_exercise/taxi",json.dumps(data))   #Senden zum server
-    
-    
-     if msg[0] == "hshl/mqtt_exercise/taxi/get_postion":
-            
-     coordinates1 = cordinates1 + randint(1, 4)
-     data = {
-         "id" : 5
-         "type" : taxi
-      "postion": coordinates1, #Coordiante vom Zielort
-     
-     }
-     client.publish("hshl/mqtt_exercise/server/set_postion,json.dumps(data)
-    
+    "id": "register",
+    "name": name,
+    "coordinates": coordinates
+    }
+    send("hshl/mqtt_exercise/taxi", json.dumps(data))
+    receive()
 
-    client.loop_forever()
+registration()
