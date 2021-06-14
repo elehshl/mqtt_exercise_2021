605fbcea4968c2958aa1951ccd5204fd1f7d1160
LordFlonni <43642299+LordFlonni@users.noreply.github.com>
2021-06-14 18:09:49 +0200

    Update Ambulance.py
    Name Error form Literal Error
    Change:  coordiantes to coordinates

--- code/services/Ambulance.py
+++ code/services/Ambulance.py
@@ -64,7 +64,7 @@ class ambulance():
     client.on_message = on_message
 
     client.connect("test.mosquitto.org", 1883, 60)
-    
+
     zahly = randint(0, 4)
     zahlx = randint(0, 4)
     coordinates = str(zahly)+";"+str(zahlx)
@@ -72,7 +72,7 @@ class ambulance():
     data = {
      "ID": " ",
      "car_name": "firefighter",
-     "coordinaten": coordiantes,
+     "coordinaten": coordinates,
      "topic": "hshl/mqtt_exercise/services/ambulance"
     }
 
