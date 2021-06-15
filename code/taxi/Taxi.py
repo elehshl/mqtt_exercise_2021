dde130f0a752bdf48b009ac068bceac144f20d1f
VisioNyrix <43642158+VisioNyrix@users.noreply.github.com>
2021-06-15 15:17:00 +0200

    Update Taxi.py

--- code/taxi/Taxi.py
+++ code/taxi/Taxi.py
@@ -1,86 +1,124 @@
 import paho.mqtt.client as mqtt
+import time
 import json
 from random import *
-BROKER_ADDRESS = "test.mosquitto.org"
+
+SERVER_ADDRESS = "test.mosquitto.org"
+TOPIC = ""
 PORT = 1883
-import time
-id = ""
+coor = ""
 name = ""
+id = ""
 
+subtopic = ["hshl/mqtt_exercise/taxi/back","hshl/mqtt_exercise/get_position"]
 
+def dataVerification(msg): #prüfung der gesendeten daten ist die datei leer = false
+    if len(json.loads(str(msg))) > 0 :
+        return True
+    else:
+        return False
+        pass
 
-print("gib einen namen ein:")
-name = input()
-
-def send(topic, object):
+def send(object,topic): #senden
+    time.sleep(2)
     client = mqtt.Client("client")
-    client.connect(BROKER_ADDRESS, PORT)
+    client.connect(SERVER_ADDRESS, PORT)
     name = object
-    print("Taxi")
-    def __init__(self, name):
-            self.name = name
-    print(" SEND Connected to MQTT Broker: " + BROKER_ADDRESS)
+    print("TAXI")
+    def __init__(self,name):
+        self.name = name
+        print(" SEND Connected to MQTT Broker: " + SERVER_ADDRESS)
     msg = str(name)
     print(msg)
     client.publish(topic, msg)
     client.loop()
 
-def receive():
-    global id
+def receive(): #empfangen
     temp = []
-    client = mqtt.Client("taxi")
-    client.connect(BROKER_ADDRESS, PORT)
+    client = mqtt.Client()
+    client.connect(SERVER_ADDRESS, PORT)
     def on_message(client, userdata, message):
         msg = str(message.payload.decode("utf-8"))
         print("message received: ", msg)
         print("message topic: ", message.topic)
-        print(msg)
         temp =  [message.topic,msg]
-        processing(temp)
-
+        if dataVerification(msg) == True:
+            messageprocessing(temp)
+        else:
+            print("Message = Leer!")
 
     def on_connect(client, userdata, flags, rc):
-        print("receive Connected to MQTT Broker: " + BROKER_ADDRESS)
-        client.subscribe("hshl/mqtt_exercise/taxi/back",2)
-        client.subscribe("hshl/mqtt_exercise/taxi/"+str(id)+"/order/back", 2)
-
+        print("Server Connected to MQTT Broker: " + SERVER_ADDRESS)
+        for i in range(0,len(subtopic)): # neue topics
+            print(str(subtopic[i]))
+            client.subscribe(subtopic[i], 2)
     client.on_connect = on_connect
     client.on_message = on_message
-    client.loop_start()
-    time.sleep(5)
-    client.loop_stop()
-
-def processing(array):
-    global id
-    js = json.loads(array[1])
-    if "hshl/mqtt_exercise/taxi/back" == array[0] and str(js['name']) == str(name):
-        id = js['id']
+    client.loop_forever()
 
 
-zahly = randint(0, 4)
-zahlx = randint(0, 4)
-coordinates = str(zahly)+";"+str(zahlx)
-
-
-def registration():
+def messageprocessing(msg):
     global name
-    data = {
+    global id
+    data = ""
+
+    if msg[0] == "hshl/mqtt_exercise/taxi/back/" and str(name) == js['name']:  # empfangen der id
+        id = str(js['id']) # speichern der erhaltenen id in die lokale(aber global) id
+        subtopic.append("hshl/mqtt_exercise/taxi/"+str(id)+"/call")
+        receive() #warte auf nachricht
+    elif msg[0] == "hshl/mqtt_exercise/taxi/"+str(id)+"/call":
+        pickUpCoordinates(js['coordinates'])
+        data = {
+        "id":id,
+        "msg": "Arrival",
+        "coordinates": js['coordinates']
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/taxi/"+str(id)+"/call/back")
+        subtopic.append("hshl/mqtt_exercise/taxi/"+str(id)+"/call/destination")
+        receive() #warten auf antwort
+    elif msg[0] == "hshl/mqtt_exercise/taxi/"+str(id)+"/call/destination":
+        driveDestination(js['destination'],js['name'])
+        data ={
+        "id":id,
+        "msg": "Arrival at destination",
+        "coordinates": js['destination']
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/taxi/"+id+"/call/destination/back")
+        receive()
+    elif msg[0] == "hshl/mqtt_exercise/get_position" and str(js['id']) == str(id):
+        data={
+        "id":id,
+        "name":name,
+        "coordinates":coor
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/set_position")
+
+def driveDestination(destinationcoor,guestname):
+    print("New destination, drive "+guestname+" to: "+destinationcoor)
+    coor = destinationcoor
+    time.sleep(1)
+    print("Arrival at: "+destinationcoor)
+
+def rndCoordinates():
+    x = randint(0,4)
+    y = randint(1,4)
+    return str(y)+";"+str(x)
+def register():
+    global coor
+    global name
+    coor = rndCoordinates()
+    data={
     "id": "register",
     "name": name,
-    "coordinates": coordinates
+    "coordinates":coor
     }
-    send("hshl/mqtt_exercise/taxi", json.dumps(data))
-    receive()
-registration()
+    send(json.dumps(data), "hshl/mqtt_exercise/taxi")
 
-
-def receive_user():                                                           # Erhalten der Coordinaten vom User (Standort + Ziel)
- if msg[0] == "hshl/mqtt_exercise/user/"+ str(js['coordinates']):
-  storePosition(js["coordinates"],js["coordinates_ziel"])
-receive_user()
-
-def receive_server():                                                         #Anfrage vom Server, senden der Coordinaten vom Standort Taxi
- if msg[0] == "hshl/mqtt_exercise/server/"+ str(js['get_coordinates']):
-   new_coordinates = coordinates_ziel + zahly
-   send("hshl/mqtt_exercise/server/set_coordinates", json.dumps(new_coordinates))
-receive_server()
+def pickUpCoordinates(pickupcoor):
+    print("New destination: "+pickupcoor)
+    coor = pickupcoor
+    time.sleep(1)
+    print("Arrival at: "+pickupcoor)
+name = input()
+register()
+receive()
