61e56ba46d16ffc875c891c1b09fea6b9c18b9da
JonasGerken <43642021+JonasGerken@users.noreply.github.com>
2021-06-15 15:46:21 +0200

    Update Ambulance.py

--- code/services/Ambulance.py
+++ code/services/Ambulance.py
@@ -1,81 +1,120 @@
 import paho.mqtt.client as mqtt
 import json
 from random import *
+BROKER_ADDRESS = "test.mosquitto.org"
+PORT = 1883
+import time
+id = ""
+name = ""
+coor = ""
+subtopic = [
+"hshl/mqtt_exercise/services/ambulance/back",
+"hshl/mqtt_exercise/get_position"
+]
 
 
-class ambulance():
+def send(topic, object): #senden
+    client = mqtt.Client("client")
+    client.connect(BROKER_ADDRESS, PORT)
+    name = object
+    print("Police")
+    def __init__(self, name):
+            self.name = name
+    print(" SEND Connected to MQTT Broker: " + BROKER_ADDRESS)
+    msg = str(name)
+    print(msg)
+    client.publish(topic, msg)
+    client.loop()
 
-    def  __init__(self, car_name, coordinaten):
-         self.car_name = car_name
-         self.coordinaten = coordinaten
-
-    ambulances = []
-
-    def processing(msg):
-        global id
-        global idCar
-        global count
-        data = ""
-        Abfrage = 3
-        print(msg[3])
-        #die erhaltene id verarbeiten
-        js = json.loads(msg[3])
-        if msg[0]=="hshl/mqtt_exercise/ambulance/back" and js['name'] == "Hans":
-            id = js['id']
-            print(id)
-            pass
-        elif msg[0] == "hshl/mqtt_exercise/set_position":
-            storePosition(js["id"],js["type"],js["coordinates"])
-
-    def requestPosition(idCar,type):
-        name =""
-        for i in range(0,len(type)):
-            pass
-        data ={
-        "id":idCar,
-        "name": name
-        }
-        send(data,"hshl/exercise/get_position")
-
-    def getid():
-      data = {
-       "id": "register",
-       "name": "Hans",
-       "coordinates": coordinates
-       }
-      send(json.dumps(data),"hshl/mqtt_exercise/user")
-      receive()
-
-
-    def on_connect(client, userdata, flags, rc):
-
-        client.subscribe("hshl/mqtt_exercise/services/ambulance", 2)
-
-        client.subscribe("service/server", 2)
-
-
-    def on_message(client, userdata, msg):
-        print(str(msg.payload))
+def receive(): #empfangen
+    global id
+    temp = []
+    client = mqtt.Client("ambulance")
+    client.connect(BROKER_ADDRESS, PORT)
+    def on_message(client, userdata, message):
+        msg = str(message.payload.decode("utf-8"))
+        print("message received: ", msg)
+        print("message topic: ", message.topic)
+        print(msg)
         temp =  [message.topic,msg]
         processing(temp)
 
-    client = mqtt.Client()
+
+    def on_connect(client, userdata, flags, rc):
+        print("Server Connected to MQTT Broker: " + BROKER_ADDRESS)
+        for i in range(0,len(subtopic)):        # hier werden bei jedem lese befehl die neuen topics aufgenommen
+            print(str(subtopic[i]))
+            client.subscribe(subtopic[i], 2)
+
     client.on_connect = on_connect
     client.on_message = on_message
+    client.loop_forever()
 
-    client.connect("test.mosquitto.org", 1883, 60)
+def processing(msg):
+    global id
+    global name
+    js = json.loads(msg[1])
+    if "hshl/mqtt_exercise/services/ambulance/back" == msg[0] and str(js['name']) == str(name):
+        id = js['id']
+        print(id)
+    elif msg[0] == "hshl/mqtt_exercise/services/ambulance/"+str(id)+"/call":                      #koordinaten des users
+        drivetoUser(js['coordinates'])    #aufruf der methode um zum user zu fahren dafür werden die koordinaten benötigt
+        data = {
+        "id":id,
+        "msg": "Arrival",
+        "coordinates": js['coordinates']
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/services/ambulance/"+str(id)+"/call/back") # senden der nachricht
+        subtopic.append("hshl/mqtt_exercise/services/ambulance/"+str(id)+"/call/destination") #aufnehmen des neuen topics in das topic array
+        receive() #warten auf antwort
+    elif msg[0] == "hshl/mqtt_exercise/services/ambulance/"+str(id)+"/call/destination":      #zielkoordinaten
+        userDestination(js['destination'],js['name']) #fahren zum wunschzuiel des kunden benötigt werden die korrdinaten und name
+        data ={
+        "id":id,
+        "msg": "Arrival at destination",
+        "coordinates": js['destination']
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/services/ambulance/"+id+"/call/destination/back") # senden der erreicht nachricht für das ziel
+        receive()#warten auf die nachricht des servers zum bekommen der position
+    elif msg[0] == "hshl/mqtt_exercise/get_position" and str(js['id']) == str(id): #neue position für den server
+        data={
+        "id":id,
+        "name":name,
+        "coordinates":coor
+        }
+        send(json.dumps(data),"hshl/mqtt_exercise/set_position")
 
+def userDestination(destinationcoor, guestname):
+    print("New destination, drive "+guestname+" to: "+destinationcoor) #textausgabe
+    coor = destinationcoor #setzen der zielkoordinaten in den standort des fahrzeugs
+    time.sleep(1) #schlafen eine sekunde
+    print("Arrival at: "+destinationcoor)
+
+def drivetoUser(usercoor):
+    print("New destination: "+usercoor) #textausgabe
+    coor = usercoor   #setzen der pickup koorindanten zu dem standkoordinaten
+    time.sleep(1)#warten
+    print("Arrival at: "+usercoor) #textausgabe
+
+def ambulancecoor():
     zahly = randint(0, 4)
     zahlx = randint(0, 4)
-    coordinates = str(zahly)+";"+str(zahlx)
+    return str(zahly)+";"+str(zahlx)
+
+
+def registration():
+    global coor
+    global name
+    coor = ambulancecoor()
 
     data = {
-     "ID": " ",
-     "car_name": "firefighter",
-     "coordinaten": coordinates,
-     "topic": "hshl/mqtt_exercise/services/ambulance"
+    "id": "register",
+    "name": name,
+    "coordinates": coor
     }
+    send("hshl/mqtt_exercise/services/ambulance", json.dumps(data))
 
-    client.publish("hshl/mqtt_exercise/services/ambulance", json.dumps(data))   #Senden zum server
-
-    client.loop_forever()
+print("Gib einen Namen ein:")
+name = input()
+registration()
+receive()
